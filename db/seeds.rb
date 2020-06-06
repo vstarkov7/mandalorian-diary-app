8fa14ade0acba36129ffe0724372f60fdb428e98
Vyacheslav Starkov <vyacheslavstarkov@Vyacheslavs-MBP.home>
2020-06-06 16:46:54 -0400

    fixed post and topic models and added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.destroy_all
+Topic.destroy_all
+
+sports = Topic.create!(name:"Sports")
+politics = Topic.create!(name:"Politics")
+nature = Topic.create!(name:"Nature")
+
+p "#{Topic.count} topics were created"
+
+Post.create!(title: 'Footy', topics: [sports])
+Post.create!(title: 'yo', topics: [politics])
+Post.create!(title: 'hahaha', topics: [politics, sports])
+
+p "#{Post.count} posts were created"
